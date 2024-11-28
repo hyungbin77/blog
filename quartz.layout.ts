6da47a2a0407b864107f724caae142f5d3ac0eee
hyungbin77 <mhkphb1-@naver.com>
2024-11-28 10:36:57 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,8 +32,12 @@ export const defaultContentPageLayout: PageLayout = {
     //Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    //Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    //Component.Backlinks(),
+  ],
+  afterBody: [
+    Component.Graph(),
     Component.Backlinks(),
   ],
 }
